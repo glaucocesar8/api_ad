5004ecbe4699e95f61d3bea421628fe1d4d30a91
glaucocesar8 <43621426+glaucocesar8@users.noreply.github.com>
2024-07-04 14:56:31 -0400

    Update credenciais.py

--- credenciais.py
+++ credenciais.py
@@ -2,9 +2,12 @@ from ldap3 import Connection, Server
 
 ##### CONEXÃO COM O AD ######
 
-server = Server("ldaps://intra.epgnet.com.br", use_ssl=True)
-conexao = Connection(server, "EPGNET\\nia", "uV<9>Ft7LK0t8", auto_bind=True)
+server = Server("ldaps://contoso.com.br", use_ssl=True)
+
+### Troque "user" por um usuário com privilégios, e coloque a senha em "senha_user"  ###
+conexao = Connection(server, "CONTOSO\user", "senha_user", auto_bind=True)
 
 ##### AUTENTICAÇÃO API #####
 
-token_api = "nwpaqnw7SvSY45Dry8i6520498495b49"
+### Crie seu token e coloque aqui ###
+token_api = "seu_token"
